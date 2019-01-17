0d08fcc532601c0cdd3c8b9bda71d59f229502f9
jwang1122 <wangqianjiang@live.com>
2019-01-17 16:34:37 -0600

    add while loop

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,13 @@
 public class Hello {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 5; i++) {
-			System.out.println( i + ": Hello, the World!");
+			System.out.println( i + ": Hello, the World! (for-loop)");
+		}
+		
+		boolean flag = true;
+		while(flag ) {
+			System.out.println("Hello, the World! (while-loop)");			
+			flag = false;
 		}
 	}
 }
