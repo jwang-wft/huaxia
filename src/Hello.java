ff3e0d998faab6f60639b6bfba8de397055ca921
jwang1122 <wangqianjiang@live.com>
2019-01-17 17:41:16 -0600

    Use String.format()

--- src/Hello.java
+++ src/Hello.java
@@ -1,14 +1,15 @@
 public class Hello {
 	static String helloWorld = "Hello, the World!";
+	
 	public static void main(String[] args) {
 		for (int i = 1; i <= 5; i++) {
-			System.out.println(i + helloWorld);
+			System.out.println(String.format("%d: %s (%s)", i, helloWorld, "for-loop"));
 		}
 
 		int i = 1;
 		boolean flag = true;
 		while (flag) {
-			System.out.println(i + helloWorld);
+			System.out.println(String.format("%d: %s (%s)", i, helloWorld, "while-loop"));
 			if (i++ >= 5) {
 				flag = false;
 			}
