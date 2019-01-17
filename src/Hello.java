b1975b180b539727044ff62f28e43ed61edf05a6
jwang1122 <wangqianjiang@live.com>
2019-01-17 16:57:24 -0600

    class level attribute

--- src/Hello.java
+++ src/Hello.java
@@ -1,13 +1,14 @@
 public class Hello {
+	static String helloWorld = "Hello, the World!";
 	public static void main(String[] args) {
 		for (int i = 1; i <= 5; i++) {
-			System.out.println(i + ": Hello, the World! (for-loop)");
+			System.out.println(i + helloWorld);
 		}
 
 		int i = 1;
 		boolean flag = true;
 		while (flag) {
-			System.out.println(i + ": Hello, the World! (for-loop)");
+			System.out.println(i + helloWorld);
 			if (i++ >= 5) {
 				flag = false;
 			}
