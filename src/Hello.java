712a6fe3a962b38700e7974d39df2361119b8510
jwang1122 <wangqianjiang@live.com>
2019-01-17 16:50:40 -0600

    while loop also print out 5 lines.

--- src/Hello.java
+++ src/Hello.java
@@ -1,13 +1,16 @@
 public class Hello {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 5; i++) {
-			System.out.println( i + ": Hello, the World! (for-loop)");
+			System.out.println(i + ": Hello, the World! (for-loop)");
 		}
-		
+
+		int i = 1;
 		boolean flag = true;
-		while(flag ) {
-			System.out.println("Hello, the World! (while-loop)");			
-			flag = false;
+		while (flag) {
+			System.out.println(i + ": Hello, the World! (for-loop)");
+			if (i++ >= 5) {
+				flag = false;
+			}
 		}
 	}
 }
