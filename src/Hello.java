fef7bbda52f66ce5ab6e0ac4d1c8db80625bb872
jwang1122 <wangqianjiang@live.com>
2019-01-17 20:05:12 -0600

    add a new method named add()

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,12 @@ public class Hello {
 	static String helloWorld = "Hello, the World!";
 	
 	public static void main(String[] args) {
+		int i1 = 5;
+		int i2 = 6;
+		Hello h = new Hello();
+		int i3 = h.add (i1, i2);
+		System.out.printf("%d + %d = %d\n", i1, i2, i3);
+		
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(String.format("%d: %s (%s)", i, helloWorld, "for-loop"));
 		}
@@ -14,5 +20,9 @@ public class Hello {
 				flag = false;
 			}
 		}
+	}
+	
+	public int add (int i1, int i2) {
+		return i1 + i2;
 	}
 }
