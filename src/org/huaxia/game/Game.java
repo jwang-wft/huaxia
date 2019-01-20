07a365f1e0a25889c475ac24286e48effac4f1e5
jwang1122 <wangqianjiang@live.com>
2019-01-20 09:02:40 -0600

    Create a Game loop, a lot of FPS: print out on the screen when start up the Game.

--- src/org/huaxia/game/Game.java
+++ src/org/huaxia/game/Game.java
@@ -3,11 +3,17 @@ package org.huaxia.game;
 import java.awt.Canvas;
 
 public class Game extends Canvas implements Runnable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 515207149561806841L;
 	private static final int WIDTH = 640;
 	private static final int HEIGHT = WIDTH / 12 * 9;
+	private Thread thread;
+	private boolean running = false;
 
 	public Game() {
-		new Window(WIDTH, HEIGHT, "My Game", this);
+		new Window(WIDTH, HEIGHT, "梓琦游戏", this);
 
 	}
 
@@ -16,12 +22,52 @@ public class Game extends Canvas implements Runnable {
 	}
 
 	public void start() {
-		// TODO Auto-generated method stub
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+	}
 
+	public synchronized void stop() {
+		try {
+			thread.join();
+			running = false;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				tick();
+				delta--;
+			}
+			if (running)
+				render();
+			frames++;
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();
+	}
+
+	private void tick() {
 
 	}
 
+	private void render() {
+
+	}
 }
