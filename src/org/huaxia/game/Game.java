378aa7aa291375303aebab0f441bab3b1de5c670
jwang1122 <wangqianjiang@live.com>
2019-01-20 09:38:02 -0600

    Fill up the game window with color

--- src/org/huaxia/game/Game.java
+++ src/org/huaxia/game/Game.java
@@ -1,6 +1,9 @@
 package org.huaxia.game;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable {
 	/**
@@ -68,6 +71,15 @@ public class Game extends Canvas implements Runnable {
 	}
 
 	private void render() {
-
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		g.dispose();
+		bs.show();
 	}
 }
