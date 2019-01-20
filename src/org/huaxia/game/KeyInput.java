846bf1f53ee0879503df2c1f732752d6cf3f5208
jwang1122 <wangqianjiang@live.com>
2019-01-20 11:59:59 -0600

    use w,s,a,d key move player around, and use up, down, left and right arrow to move player2 around.

--- src/org/huaxia/game/KeyInput.java
+++ src/org/huaxia/game/KeyInput.java
@@ -15,8 +15,16 @@ public class KeyInput extends KeyAdapter {
 		for (int i = 0; i<handler.object.size(); i++) {
 			GameObject tempObject = handler.object.get(i);
 			if(tempObject.getId() == ID.Player) {
-				if(key==KeyEvent.VK_W)
-					tempObject.setY(tempObject.getY() - 1);
+				if(key==KeyEvent.VK_W) tempObject.setVelY(-5);
+				if(key==KeyEvent.VK_S) tempObject.setVelY(5);
+				if(key==KeyEvent.VK_D) tempObject.setVelX(5);
+				if(key==KeyEvent.VK_A) tempObject.setVelX(-5);
+			}
+			if(tempObject.getId() == ID.Player2) {
+				if(key==KeyEvent.VK_UP) tempObject.setVelY(-5);
+				if(key==KeyEvent.VK_DOWN) tempObject.setVelY(5);
+				if(key==KeyEvent.VK_RIGHT) tempObject.setVelX(5);
+				if(key==KeyEvent.VK_LEFT) tempObject.setVelX(-5);
 			}
 		}
 	}
@@ -24,5 +32,20 @@ public class KeyInput extends KeyAdapter {
 	public void keyReleased(KeyEvent e) {
 		int key = e.getKeyCode();
 
+		for (int i = 0; i<handler.object.size(); i++) {
+			GameObject tempObject = handler.object.get(i);
+			if(tempObject.getId() == ID.Player) {
+				if(key==KeyEvent.VK_W) tempObject.setVelY(0);
+				if(key==KeyEvent.VK_S) tempObject.setVelY(0);
+				if(key==KeyEvent.VK_D) tempObject.setVelX(0);
+				if(key==KeyEvent.VK_A) tempObject.setVelX(0);
+			}
+			if(tempObject.getId() == ID.Player2) {
+				if(key==KeyEvent.VK_UP) tempObject.setVelY(0);
+				if(key==KeyEvent.VK_DOWN) tempObject.setVelY(0);
+				if(key==KeyEvent.VK_RIGHT) tempObject.setVelX(0);
+				if(key==KeyEvent.VK_LEFT) tempObject.setVelX(0);
+			}
+		}
 	}
 }
