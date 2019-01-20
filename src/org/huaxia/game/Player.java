f7cacc483ad543947e8df9058a24cf85d8bef801
jwang1122 <wangqianjiang@live.com>
2019-01-20 10:21:23 -0600

    Plays move from left to right.

--- src/org/huaxia/game/Player.java
+++ src/org/huaxia/game/Player.java
@@ -7,11 +7,13 @@ public class Player extends GameObject {
 
 	public Player(int x, int y, ID id) {
 		super(x, y, id);
+		velX = 1;
 	}
 
 	@Override
 	public void tick() {
-		// TODO Auto-generated method stub
+		x += velX;
+		y += velY;
 
 	}
 
