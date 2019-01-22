3439300b424185dc8d692f8342c5936d4ffd6394
jwang1122 <wangqianjiang@live.com>
2019-01-22 07:37:34 -0600

    Make Key response smoothly

--- src/org/huaxia/game/KeyInput.java
+++ src/org/huaxia/game/KeyInput.java
@@ -5,9 +5,14 @@ import java.awt.event.KeyEvent;
 
 public class KeyInput extends KeyAdapter {
 	private Handler handler;
-
+	private boolean[] keyDown = new boolean[4];
+	
 	public KeyInput(Handler handler) {
 		this.handler = handler;
+		keyDown[0] = false;
+		keyDown[1] = false;
+		keyDown[2] = false;
+		keyDown[3] = false;
 	}
 
 	public void keyPressed(KeyEvent e) {
@@ -15,10 +20,10 @@ public class KeyInput extends KeyAdapter {
 		for (int i = 0; i<handler.object.size(); i++) {
 			GameObject tempObject = handler.object.get(i);
 			if(tempObject.getId() == ID.Player) {
-				if(key==KeyEvent.VK_W) tempObject.setVelY(-5);
-				if(key==KeyEvent.VK_S) tempObject.setVelY(5);
-				if(key==KeyEvent.VK_D) tempObject.setVelX(5);
-				if(key==KeyEvent.VK_A) tempObject.setVelX(-5);
+				if(key==KeyEvent.VK_W) {tempObject.setVelY(-5); keyDown[0]=true;}
+				if(key==KeyEvent.VK_S) {tempObject.setVelY(5); keyDown[1]=true;}
+				if(key==KeyEvent.VK_D) {tempObject.setVelX(5); keyDown[2]=true;}
+				if(key==KeyEvent.VK_A) {tempObject.setVelX(-5); keyDown[3]=true;}
 			}
 		}
 		if(key==KeyEvent.VK_ESCAPE) System.exit(0);
@@ -30,10 +35,12 @@ public class KeyInput extends KeyAdapter {
 		for (int i = 0; i<handler.object.size(); i++) {
 			GameObject tempObject = handler.object.get(i);
 			if(tempObject.getId() == ID.Player) {
-				if(key==KeyEvent.VK_W) tempObject.setVelY(0);
-				if(key==KeyEvent.VK_S) tempObject.setVelY(0);
-				if(key==KeyEvent.VK_D) tempObject.setVelX(0);
-				if(key==KeyEvent.VK_A) tempObject.setVelX(0);
+				if(key==KeyEvent.VK_W) keyDown[0]=false; //tempObject.setVelY(0);
+				if(key==KeyEvent.VK_S) keyDown[1]=false; // tempObject.setVelY(0);
+				if(key==KeyEvent.VK_D) keyDown[2]=false; // tempObject.setVelX(0);
+				if(key==KeyEvent.VK_A) keyDown[3]=false; // tempObject.setVelX(0);
+				if(keyDown[0] && keyDown[1]) tempObject.setY(0);
+				if(keyDown[2] && keyDown[3]) tempObject.setX(0);
 			}
 		}
 	}
